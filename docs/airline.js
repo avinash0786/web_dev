fc476b22e7ab9bdae3de31e21d214412da9e5ac9
avinash kumar <44425342+avinash0786@users.noreply.github.com>
2020-08-27 23:01:13 +0530

    FRontEnd CA

---
+++ docs/airline.js
@@ -0,0 +1,10 @@
+console.log("Airline js conect..")
+function mouseDown(obj)
+{
+    console.log("FUnction clicked");
+    obj.style.border="0 0 2px 0 solid #06a7f1";
+}
+function mouseUp(obj){
+    console.log("Mouse up")
+    // obj.style.border="0px 0px 1.5px 0px solid #C5012D";
+}
